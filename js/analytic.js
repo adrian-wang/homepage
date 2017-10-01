e8646ddde75c99d6ae9a985f69d0ecfc9aa0a6c1
Daoyuan Wang <me@daoyuan.wang>
2017-10-02 00:56:45 +0800

    encoding

--- js/analytic.js
+++ js/analytic.js
@@ -10,7 +10,7 @@ function ga_async_load(fun){
 ga_async_load( function(){
     var s = document.createElement('script');
     s.type = 'text/javascript';
-    s.async = true; //js下载之后立刻动作
+    s.async = true;
     s.innerHTML = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ \
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), \
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) \
